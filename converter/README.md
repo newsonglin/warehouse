7a1beb3790ad425f1507c334fc2f315e2cad3c2d
Songlin Li <newsonglin@gmail.com>
2021-11-18 16:15:57 +0800

    Migrate Java from 1.8 to 11

--- converter/README.md
+++
@@ -1,8 +0,0 @@
-##### To build an executable jar, use mvn command below
-```shell script
-mvn clean compile assembly:single
-```
-
-
-
-
--- converter/src/main/java/com/exigen/hot/interfaces/domian/CarryUnit.java
+++
@@ -1,9 +0,0 @@
-package com.exigen.hot.interfaces.domian;
-
-/**
- * Created by gtang on 2020/1/7.
- */
-public enum CarryUnit {
-    P,
-    T;
-}
